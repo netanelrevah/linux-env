6a4edd8ea0b91492e68d57fb8d554b7783132f8f
Netanel Revah <netanelrevah@outlook.com>
2016-07-08 15:46:15 +0300

    Add configs for tmux vim and git

---
+++ install.sh
@@ -0,0 +1,21 @@
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get install tmux
+sudo apt-get install git
+
+mv ~/.vimrc ~/.vimrc.bkp
+ln -s .vimrc ~/.vimrc
+
+mv ~/.tmux.conf ~/.tmux.conf.bkp
+ln -s .tmux.conf ~/.tmux.conf
+
+# Configure Git
+# Global Username and Email as the install repo
+git config --global user.name `git config user.name`
+git config --global user.email `git config user.email`
+
+# SVN like Alises
+git config --global alias.co checkout
+git config --global alias.st status
+git config --global alias.ci commit
+git config --global alias.last 'log -1 HEAD'
